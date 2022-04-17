f7bf84e020e80ae0fd9f9a5f76699116b371833b
190340054 <190340054@klh.edu.in>
2022-04-17 08:41:22 +0530

    POST API in Json formate in console

---
+++ post_api/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PostFrom from './components/PostAPI';
+
+function App() {
+  return (
+    <div className="App">
+      <PostFrom></PostFrom>
+    </div>
+  );
+}
+
+export default App;
